6f81218af814617ab2ba0421fc2b59e15f7f8435
CanekSystemsHub <ninfax@hotmail.com>
2021-03-09 17:07:19 -0600

    Create new VM via AZ Temaplte

--- .github/Week4_Tests/Powesheshell.ps1
+++ .github/Week4_Tests/Powesheshell.ps1
@@ -1,2 +1,9 @@
 
-Get-AzVM -ResourceGroupName FinanceEast -Name 'QTC-APP123'
+#Get-AzVM -ResourceGroupName FinanceEast -Name 'QTC-APP123'
+
+$rg="learn-quickstart-vm-rg"
+
+New-AzResourceGroupDeployment -Name CanekVm1 -ResourceGroupName $rg -TemplateUri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-windows/azuredeploy.json" `
+                                -adminUsername canekadmin -dnsLabelPrefix mydeployment-31926
+
+Get-AzResourceGroupDeployment -Name CanekVm1 -ResourceGroupName $rg

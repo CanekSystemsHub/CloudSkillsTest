c73167303188e216582e8ad06eb0f4b1b01a1ff7
CanekSystemsHub <ninfax@hotmail.com>
2021-03-25 16:55:43 -0600

    Second Change

--- .github/Week1_Tests/PhytonClass.py
+++ .github/Week1_Tests/PhytonClass.py
@@ -1,9 +1,9 @@
-Class Dog:
+class Dog:
     def __init__(self, breed, age):
             self.breed = breed
             self.age = age
 
 dogs = Dog("bulldog",1)
 
-print(dogs.breed)
-#print(dogs.age)
+#print(dogs.breed)
+print(dogs.age)
